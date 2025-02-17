5510f3622aae1e5b3552ee7e300cabda65280f95
André Melchior <andre.melchior@outlook.com>
2025-02-16 23:47:50 -0300

    feat: add code for a traffic light with LEDs

---
+++ semaforo.cpp
@@ -0,0 +1,24 @@
+// código para fazer um "semáforo" com leds
+//
+void setup()
+{
+  pinMode(1, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(1, HIGH);
+  delay(3000); // Wait for 1000 millisecond(s)
+  digitalWrite(1, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(2, HIGH);
+  delay(3000); // Wait for 1000 millisecond(s)
+  digitalWrite(2, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(3, HIGH);
+  delay(3000); // Wait for 1000 millisecond(s)
+  digitalWrite(3, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
