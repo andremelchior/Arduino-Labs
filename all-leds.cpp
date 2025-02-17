9a09c30e848e86d00edcfab585fba6bf1fad3d6a
André Melchior <andre.melchior@outlook.com>
2025-02-16 23:48:33 -0300

    feat: add code for testing all LEDs

---
+++ all-leds.cpp
@@ -0,0 +1,37 @@
+void setup()
+{
+  pinMode(0, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(0, HIGH);
+  delay(300); // Wait for 1000 millisecond(s)
+  digitalWrite(0, LOW);
+  delay(300); // Wait for 1000 millisecond(s)
+  
+    digitalWrite(1, HIGH);
+  delay(300); // Wait for 1000 millisecond(s)
+  digitalWrite(1, LOW);
+  delay(300); // Wait for 1000 millisecond(s)
+  
+    digitalWrite(2, HIGH);
+  delay(300); // Wait for 1000 millisecond(s)
+  digitalWrite(2, LOW);
+  delay(300); // Wait for 1000 millisecond(s)
+  
+    digitalWrite(3, HIGH);
+  delay(300); // Wait for 1000 millisecond(s)
+  digitalWrite(3, LOW);
+  delay(300); // Wait for 1000 millisecond(s)
+  
+    digitalWrite(4, HIGH);
+  delay(300); // Wait for 1000 millisecond(s)
+  digitalWrite(4, LOW);
+  delay(300); // Wait for 1000 millisecond(s)
+  
+    digitalWrite(5, HIGH);
+  delay(300); // Wait for 1000 millisecond(s)
+  digitalWrite(5, LOW);
+  delay(300); // Wait for 1000 millisecond(s)
+}
