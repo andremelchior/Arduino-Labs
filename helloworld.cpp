31e8a8548ccc7fb0f0395c8f05e5dc978c81d398
André Melchior <andre.melchior@outlook.com>
2025-02-16 23:45:29 -0300

    initial commit: add simple LED project code in C++ with Arduino

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+// "Hello World" com c++ no arduino
+//
+void setup()
+{
+  pinMode(8, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(8, HIGH);
+  delay(3000); // Wait for 1000 millisecond(s)
+  digitalWrite(8, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
